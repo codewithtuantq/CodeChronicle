e18993f8181e05d1b72985e09b53532e66ec8487
tqt97 <kutuanonline199@gmail.com>
2025-09-11 22:07:07 +0700

    Add starter blog page

---
+++ resources/js/pages/blogs/index.tsx
@@ -0,0 +1,20 @@
+import AppLayout from '@/layouts/guest-layout';
+import { dashboard } from '@/routes';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: dashboard().url,
+    },
+];
+
+export default function Dashboard() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Dashboard" />
+            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">Blogs</div>
+        </AppLayout>
+    );
+}
